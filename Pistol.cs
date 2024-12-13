9566028f0ad4a30ac995a3e66968b224a601a94b
Sofiya Sherif <sofiyasherif2024@gmail.com>
2024-12-12 23:43:56 -0600

    Update Pistol.cs

--- Pistol.cs
+++ Pistol.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Pistol : MonoBehaviour
 {
+// Sofiya wrote this part of the code 
     public int maxAmmoInMag = 10;       // Maximum ammo capacity in the magazine
     public int maxAmmoInStorage = 30;   // Maximum ammo capacity in the storage
     public float shootCooldown = 0.5f;  // Cooldown time between shots
@@ -67,7 +68,7 @@ public class Pistol : MonoBehaviour
             shootTimer -= Time.deltaTime;
         }
     }
-
+// Sofiya wrote this part of the code 
     void Shoot()
     {
         // Check if there is ammo in the magazine
@@ -126,7 +127,7 @@ public class Pistol : MonoBehaviour
             Debug.Log("Cannot shoot");
         }
     }
-
+// Sofiya wrote this part of the code 
     void Reload()
     {
         switchCooldown -= Time.deltaTime;
